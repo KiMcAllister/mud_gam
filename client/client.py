8370a62b68c89d10729183590032c53a50098e8c
KiMcAllister <ki.mcallister2@gmail.com>
2022-01-21 08:16:31 -0700

    Create client.py

---
+++ client/client.py
@@ -0,0 +1,26 @@
+import msvcrt
+import time
+import sys
+
+class TimeoutExpired(Exception):
+    pass
+
+def input_raw(prompt):
+    sys.stdout.write(prompt)
+    #sys.stdout.flush()
+
+def input_with_timeout(prompt, timeout, timer=time.monotonic):
+    sys.stdout.write(prompt)
+    sys.stdout.flush()
+    endtime = timer() + timeout
+    result = []
+    while timer() < endtime:
+        if msvcrt.kbhit():
+            result.append(msvcrt.getwche()) #XXX can it block on multibyte characters?
+            #if result[-1] == '\r':
+            #    return ''.join(result[:-1])
+        time.sleep(0.04) # just to yield to other processes/threads
+    raise TimeoutExpired
+while True:
+    user_in = input_with_timeout("tell me: ",100)
+print(user_in)
