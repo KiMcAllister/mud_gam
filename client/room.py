eca1b0fb62b745dc4f1844dc264cd7ef1c4657be
TheBrightReaper <74506120+TheBrightReaper@users.noreply.github.com>
2022-01-21 17:48:13 -0700

    Create room.py

---
+++ client/room.py
@@ -0,0 +1,29 @@
+class building:
+    def __init__(self, type, rooms):
+        self.type = type
+        self.rooms = rooms
+class room:
+    def __init__(self, type, furniture, windows):
+        self.type = type
+        self.furniture = furniture
+        self.windows = windows
+class furniture:
+    def __init__(self, type, loot):
+        self.type = type
+        self.loot = loot
+
+
+
+
+
+
+
+building1 = building(room(("bedroom","garage"),furniture("table","loot"),[True,False]),[])
+#house1 = building("house", [room(["bedroom","bathroom","livingroom"],["desk","chair"],[True, False])],)
+
+
+spacer = ", a "
+print(f"You are in a {building1.type}.")
+
+#print(f"Inside the {house1.rooms[0].type[0]}, there is a {house1.rooms[0].furniture[0]} in the corner. {house1.rooms[0].windows[0]}")
+#print(f"There are {len(house.rooms)} rooms inside of the {house.type}. It has a {spacer.join(house.rooms[:-1])}, and a {house.rooms[2]}")
