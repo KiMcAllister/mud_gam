50bdc50e21b3c60fe4c127ac30c6faee7ea54429
TheBrightReaper <74506120+TheBrightReaper@users.noreply.github.com>
2022-02-18 18:06:37 -0700

    Update room.py

--- client/room.py
+++ client/room.py
@@ -1,3 +1,28 @@
+
+
+spacer = ", a "
+
+buildingrooms = {"house":["bedroom", "bathroom", "living_room", "kitchen", "dining_room",]}
+
+class building:
+    def __init__(self, type):
+        self.type = type
+    def rooms(self):
+        print("The house contains {0}".format(buildingrooms[self.type]) )
+
+house = building("house")
+
+house.rooms()
+
+#demo building will be a house
+
+
+
+"""
+
+
+
+
 class building:
     def __init__(self, type, rooms):
         self.type = type
@@ -27,3 +52,5 @@ print(f"You are in a {building1.type}.")
 
 #print(f"Inside the {house1.rooms[0].type[0]}, there is a {house1.rooms[0].furniture[0]} in the corner. {house1.rooms[0].windows[0]}")
 #print(f"There are {len(house.rooms)} rooms inside of the {house.type}. It has a {spacer.join(house.rooms[:-1])}, and a {house.rooms[2]}")
+
+"""
